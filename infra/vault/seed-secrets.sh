ddc5ddfd66c5384d84cc1d8282d9bd4415f60756
tyagiakshit171 <tyagiakshit171@gmail.com>
2026-08-16 15:24:51 +0530

    Adding support for hashicorp Vault infra

---
+++ infra/vault/seed-secrets.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+echo "Seeding Vault secrets..."
+
+vault kv put secret/notification-engine/common \
+  DB_USERNAME="$DB_USERNAME" DB_PASSWORD="$DB_PASSWORD" REDIS_PASSWORD="$REDIS_PASSWORD"
+
+vault kv put secret/notification-engine/email-consumer \
+  SENDGRID_API_KEY="$SENDGRID_API_KEY" SENDGRID_FROM_EMAIL="$SENDGRID_FROM_EMAIL"
+
+vault kv put secret/notification-engine/sms-consumer \
+  TWILIO_ACCOUNT_SID="$TWILIO_ACCOUNT_SID" TWILIO_AUTH_TOKEN="$TWILIO_AUTH_TOKEN" TWILIO_FROM_SMS_NUMBER="$TWILIO_FROM_SMS_NUMBER"
+
+vault kv put secret/notification-engine/whatsapp-consumer \
+  TWILIO_ACCOUNT_SID="$TWILIO_ACCOUNT_SID" TWILIO_AUTH_TOKEN="$TWILIO_AUTH_TOKEN" TWILIO_FROM_WHATSAPP_NUMBER="$TWILIO_FROM_WHATSAPP_NUMBER"
+
+echo "Done."
